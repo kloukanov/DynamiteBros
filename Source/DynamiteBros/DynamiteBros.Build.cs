ba32f275c6a02cab7da04cc9b16872c0c2c7dbac
Krassy Loukanov <kloukanov@outlook.com>
2024-04-14 17:17:27 -0400

    initial commit

---
+++ Source/DynamiteBros/DynamiteBros.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class DynamiteBros : ModuleRules
+{
+	public DynamiteBros(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+	}
+}
